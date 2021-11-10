4a5f717899072fb948168638481b5dced5943822
slowede <79045490+slowede@users.noreply.github.com>
2021-11-10 09:59:12 +0100

    Player GUI

--- main_jframe.java
+++ main_jframe.java
@@ -1,15 +1,38 @@
 import java.awt.Color;
+import java.util.Scanner;
 
 import javax.swing.*;
 
 public class main_jframe extends zehntausend {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Anzahl der Spieler");
+        int anzahl_spieler = 6;
+
+        Player[] player = new Player[anzahl_spieler];
+
+        // Schleife zum Erstellen der Spieler
+        for (int i = 0; i < anzahl_spieler; i++) {
+            player[i] = new Player();
+            System.out.println("\n");
+        }
+
         JFrame frame = new JFrame();
-        JPanel player = new JPanel();
+        JPanel player_panel = new JPanel();
+        JPanel button_panel = new JPanel();
+        JLabel[] player_label_name = new JLabel[6];
+        // JLabel[] player_label_points = new JLabel[6];
         JPanel[] list = new JPanel[6];
 
         for (int i = 0; i < 6; i++) {
             list[i] = new JPanel();
+        }
+
+        for (int i = 0; i < 6; i++) {
+            player_label_name[i] = new JLabel();
+            player_label_name[i].setText("<html>Name: " + player[i].getName() + "<br>Points: "+ Integer.toString(player[i].getPoints()) + "</html>");
+            list[i].add(player_label_name[i]);
+            // list[i].add(player_label_points[i]);
         }
 
         int list_distance = 0;
@@ -18,12 +41,16 @@ public class main_jframe extends zehntausend {
             list[i].setBounds(list_distance, 25, 200, 200);
             list_distance += 208;
             list[i].setBackground(new Color(0x7195A0));
-            player.add(list[i]);
+            player_panel.add(list[i]);
         }
 
-        player.setLayout(null);
-        player.setBounds(0, 10, 1250, 250);
-        player.setBackground(new Color(0xAEB081));
+        player_panel.setLayout(null);
+        player_panel.setBounds(0, 10, 1250, 250);
+        player_panel.setBackground(new Color(0xAEB081));
+
+        button_panel.setLayout(null);
+        button_panel.setBounds(0, 275, 1250, 425);
+        button_panel.setBackground(Color.white);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Game");
@@ -31,7 +58,8 @@ public class main_jframe extends zehntausend {
         frame.getContentPane().setBackground(new Color(0xC9A58D));
         frame.setOpacity(1f);
         frame.setSize(1250, 750);
-        frame.add(player);
+        frame.add(player_panel);
+        frame.add(button_panel);
         frame.setResizable(false);
         frame.setVisible(true);
     }
